9c9802f75a0ceb351d7d95f706f026646be7aa77
Alex Semchenkov <zigola12@list.ru>
2021-06-22 09:41:05 +0300

    DitheringShader

---
+++ Assets/Scripts/DitherFilter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DitherFilter : SimpleFilter
+{
+    [SerializeField] private Texture2D _noiseTexture;
+    [SerializeField] private Texture2D _rampTexture;
+
+    private EdgeRender _edgeRender;
+
+    protected override void Init()
+    {
+        _edgeRender = GetComponent<EdgeRender>();
+    }
+    protected override void UseFilter(RenderTexture src, RenderTexture dst)
+    {
+        _mat.SetTexture("_NoiseTex", _noiseTexture);
+        _mat.SetTexture("_ColorRampTex", _rampTexture);
+
+        RenderTexture big = RenderTexture.GetTemporary(src.width * 2, src.height * 2);
+        RenderTexture half = RenderTexture.GetTemporary(src.width / 2, src.height / 2);
+
+        RenderTexture edged = RenderTexture.GetTemporary(src.width, src.height);
+        _edgeRender.RenderBySobel(src, edged);
+        _mat.SetTexture("_EdgedTex", edged);
+
+        Graphics.Blit(src, big);
+        Graphics.Blit(big, half, _mat);
+        Graphics.Blit(half, dst);
+
+        RenderTexture.ReleaseTemporary(big);
+        RenderTexture.ReleaseTemporary(half);
+        RenderTexture.ReleaseTemporary(edged);
+    }
+}
---
+++ Assets/Scripts/EdgeRender.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeRender : MonoBehaviour
+{
+    [SerializeField] private float _sensitivityDepth = 1.0f;
+    [SerializeField] private float _sensitivityNormals = 1.0f;
+    [SerializeField, Range(0,1)] private float _edgesOnly = 1.0f;
+    [SerializeField] private Color _edgesOnlyBgColor = Color.black;
+    [SerializeField] private Color _edgesColor = Color.white;
+
+    private Material _robertMat;
+    private Material _sobelMat;
+
+    private void Awake()
+    {
+        _sobelMat = new Material(Shader.Find("Hidden/EdgeDetection"));
+        _robertMat = new Material(Shader.Find("Hidden/EdgeDetectColors"));
+        var camera = GetComponent<Camera>();
+        camera.depthTextureMode |= DepthTextureMode.DepthNormals;
+    }
+
+    public void RenderByRobert(RenderTexture src, RenderTexture dst)
+    {
+        _robertMat.SetVector("_Sensitivity", new Vector4(_sensitivityDepth, _sensitivityNormals,
+            1.0f, _sensitivityNormals));
+        _robertMat.SetFloat("_BgFade", _edgesOnly);
+        _robertMat.SetVector("_BgColor", _edgesOnlyBgColor);
+        _robertMat.SetVector("_Color", _edgesColor);
+        Graphics.Blit(src, dst, _robertMat);
+    }
+
+    public void RenderBySobel(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, _sobelMat);
+    }
+}
